8f3109b30591669e51a83c267749ff23fb6f1518
Todd Thompson <toddthompson2@my.unt.edu>
2024-02-12 15:29:39 +0000

    hello flask

--- app.py
+++ app.py
@@ -5,3 +5,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html", title="Hello")
+
+@app.route("/hello")
+@app.route("/hello2")
+def hello():
+    return render_template("index.html", title="Hello")
